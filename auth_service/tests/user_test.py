13e76ed9befec54647dfd28453fe690a8896bf97
g1Den1s-it <g1den1splay@gmail.com>
2024-11-17 23:42:41 +0200

    added several tests

--- auth_service/tests/user_test.py
+++ auth_service/tests/user_test.py
@@ -34,6 +34,9 @@ class TestUser:
 
     @pytest.mark.asyncio
     async def test_update_user(self, ac: AsyncClient, async_connection: AsyncEngine):
+        if not TestUser.login_response:
+            assert False, "Login response is not available or None"
+
         res =  await ac.put("/auth/update/",
                             headers={"Authorization": f"Bearer {TestUser.login_response.get("access_token")}"},
                             json={"username": "new-test-username"})
@@ -43,7 +46,22 @@ class TestUser:
 
 
     @pytest.mark.asyncio
+    async def test_update_user_invalid_token(self, ac: AsyncClient, async_connection: AsyncEngine):
+        if not TestUser.login_response:
+            assert False, "Login response is not available or None"
+
+        res =  await ac.put("/auth/update/",
+                            headers={"Authorization": f"{TestUser.login_response.get("access_token")}"},
+                            json={"username": "new-test-username"})
+
+        assert res.status_code == 401
+
+
+    @pytest.mark.asyncio
     async def test_check_token(self, ac: AsyncClient):
+        if not TestUser.login_response:
+            assert False, "Login response is not available or None"
+
         res = await ac.post("/auth/check-token/",
                             json={"access_token": TestUser.login_response.get("access_token")})
 
@@ -53,9 +71,35 @@ class TestUser:
 
 
     @pytest.mark.asyncio
+    async def test_check_invalid_token(self, ac: AsyncClient):
+        if not TestUser.login_response:
+            assert False, "Login response is not available or None"
+
+        res = await ac.post("/auth/check-token/",
+                            json={"access_token": TestUser.login_response.get("access_token")+"w12e1er12"})
+
+        assert res.status_code == 200
+        assert res.json().get("access_token")['valid'] == False
+
+
+    @pytest.mark.asyncio
     async def test_refresh_token(self, ac: AsyncClient):
+        if not TestUser.login_response:
+            assert False, "Login response is not available or None"
+
         res = await ac.post("/auth/refresh-token/",
-                            json={"refresh_token": TestUser.login_response.get("refresh_token")})
+                                json={"refresh_token": TestUser.login_response.get("refresh_token")})
 
         assert res.status_code == 200
         assert res.json().get("access_token")
+
+
+    @pytest.mark.asyncio
+    async def test_refresh_invalid_token(self, ac: AsyncClient):
+        if not TestUser.login_response:
+            assert False, "Login response is not available or None"
+
+        res = await ac.post("/auth/refresh-token/",
+                                json={"refresh_token": TestUser.login_response.get("refresh_token")+"dqwd"})
+
+        assert res.status_code == 401
